bc01259c8a70b5f8c9c93992f6aeb9c0dafa5019
sapnagautom <sapnakumarippes@gmail.com>
2025-07-11 14:15:05 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,19 @@
+class largest {
+public static void main(String args[])
+{
+int a=20; int b=400; int c=304;
+if(a>b && a>c)
+{
+System.out.println("a is greater than ab and c");
+}
+else if(b>a && b>c)
+{
+System.out.println("b is greater than a and c");
+}
+else
+{
+System.out.println("c is greater than a and b");
+}
+System.out.println("thankyou");
+}
+}
