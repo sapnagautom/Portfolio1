79b7d75bf916fac0c4554ffee9b8a8b9d82a1dd3
sapnagautom <sapnakumarippes@gmail.com>
2025-07-11 14:13:01 +0530

    Add files via upload

---
+++ sumArray.java
@@ -0,0 +1,11 @@
+public class sumArray {
+public static void main(String arg[]){
+
+int number[] ={10 ,39 ,49 , 65 ,75};
+int sum =0;
+for(int num : number){
+sum += num;
+}
+System.out.println("sum of array :" + sum );
+}
+}
