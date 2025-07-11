60696a2d5420406a2ec3e61c00a140b11a5765c5
sapnagautom <sapnakumarippes@gmail.com>
2025-07-11 14:19:00 +0530

    Add files via upload

---
+++ pro5.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>php program</title>
+</head>
+<body>
+<form action ="prog5.php" method="post">
+<p> how many dic</p>
+<input type ="text" name="num">
+<p> how many faces?"</p>
+<input type ="submit" value="roll">
+<input type ="reset: value ="clear value">
+</body>
+</html>
+ 
